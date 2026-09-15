537de4faca5e1ff209847bab171f67e9ffb2bcee
flightinformation <jefflum.gfs@gmail.com>
2026-09-15 08:28:27 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /workspace
-exec streamlit run app.py \
-  --server.port 8501 \
-  --server.address 0.0.0.0 \
-  --server.headless true \
-  --server.fileWatcherType none \
-  --browser.gatherUsageStats false
